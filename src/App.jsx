d0ba1b7eeca855300e7356a7722b111dc3544a71
danishamov <122825234+danishamov@users.noreply.github.com>
2024-07-15 23:22:34 +0100

    Implement Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Contact from "./Components/contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/header/Header";
+import MatchInfo from "./Components/match-info/MatchInfo";
 
 function App() {
     return (
         <>
             <Header />
+            <MatchInfo />
+            <Contact />
+            <Footer />
         </>
     );
 }
